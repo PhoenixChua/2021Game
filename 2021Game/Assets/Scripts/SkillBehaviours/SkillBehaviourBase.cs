896cc7810f49f71a4b46b528db874fbb664a9a2b
Phoenix <phoenix.ch13@gmail.com>
2021-01-30 15:36:44 +0800

    Skill Behaviours

---
+++ 2021Game/Assets/Scripts/SkillBehaviours/SkillBehaviourBase.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillBehaviourBase : MonoBehaviour
+{
+    public Unit UserUnit;	
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+    void OnTriggerStay(Collider other)
+    {
+        Unit OtherUnit = other.gameObject.GetComponent<Unit>();
+
+        if (!OtherUnit)
+        {
+			return;
+		}
+        else if (!UserUnit || UserUnit != OtherUnit)
+        {
+			//Resolve effects;
+			Debug.Log("Bloop");
+		}			
+	}		
+}
