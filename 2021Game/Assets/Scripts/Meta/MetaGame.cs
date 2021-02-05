bc068711a20923a6a8b249539d881c8ee1bf2af6
Phoenix <phoenix.ch13@gmail.com>
2021-02-05 20:21:26 +0800

    Particle Tests

--- 2021Game/Assets/Scripts/Meta/MetaGame.cs
+++ 2021Game/Assets/Scripts/Meta/MetaGame.cs
@@ -9,12 +9,14 @@ public class MetaGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+		Cursor.visible = false;
 		_MetaObjectPool = GetComponent<MetaObjectPool>();
 		_MetaObjectPool.Instantiate("MetaGameCamera");
 		_MetaObjectPool.Instantiate("UnitPlayer");
 		_MetaObjectPool.Instantiate("UIGameMousePointer");
 		_MetaObjectPool.Instantiate("UIGameHeightMarker");
 		_MetaObjectPool.Instantiate("UnitCrate",new Vector3(14,0.5f,15));
+		_MetaObjectPool.Instantiate("UnitCrate",new Vector3(14,1.5f,15));
     }
 
     // Update is called once per frame
